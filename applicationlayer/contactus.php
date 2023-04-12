5f8022b0d59cecdcafc6d2a9d8f22785f81de3b2
darsh <98086747+dm1010@users.noreply.github.com>
2023-04-12 18:30:31 +0530

    DocOnline

---
+++ applicationlayer/contactus.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{
+    background-color:#BFBFBD;
+}
+h1{
+    top: 100%;
+    font-size: 80px;
+}
+p1{
+    font-size: 30px;
+}
+</style>
+</head>
+
+<body >
+<br>
+<img src="https://outfrontmedia.ca/-/media/images/ofmcanada/images/contact-us-envato-blocks.jpg"
+width=100%; 
+/>
+<h1><center><u>Contact Us</u></center></h1>
+<br>
+<br>
+<br>
+<p1><center>Darsh Mangukiya -------------------------------------------------    21it076@charusat.edu.in</center></p1><br>
+<p1><center>Dhruv Khalasi --------------------------------------------------------   21it065@charusat.edu.in</center></p1><br>
+<p1><center>Jugal Mehta ---------------------------------------------------------    21it083@charusat.edu.in</center></p1><br>
+<br><br><br><br>
+</body>
+
+</html>
